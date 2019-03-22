8bbfc9517428e0df431bc8360aded3e336e046ed
A.J.J. Valkenberg <anton.valkenberg@gmail.com>
2019-03-22 17:53:39 +0100

    [EvaluationStrategyHearthStone] Removed unused old HS state evaluation.

--- AVThesis/SabberStone/Strategies/EvaluationStrategyHearthStone.cs
+++ AVThesis/SabberStone/Strategies/EvaluationStrategyHearthStone.cs
@@ -109,63 +109,6 @@ namespace AVThesis.SabberStone.Strategies {
             throw new ArgumentOutOfRangeException($"Evaluation values do not fall into the expected range: oTD={oTD:F3} | rTD={rTD:F3}");
         }
 
-        /// <summary>
-        /// Returns the value of the argument state with respect to the argument node.
-        /// </summary>
-        /// <param name="context">The context of the search.</param>
-        /// <param name="node">The node that provides the context to evaluate the state.</param>
-        /// <param name="state">The state that should be evaluated.</param>
-        /// <returns>Double representing the value of the state with respect to the node.</returns>
-        [Obsolete("EvaluateOld is deprecated and replaced by Evaluate.")]
-        public double EvaluateOld(SearchContext<List<SabberStoneAction>, SabberStoneState, SabberStoneAction, object, SabberStoneAction> context, TreeSearchNode<SabberStoneState, SabberStoneAction> node, SabberStoneState state) {
-
-            var rootPlayerId = context.Source.CurrentPlayer();
-            var rootPlayer = state.Player1.Id == rootPlayerId ? state.Player1 : state.Player2;
-            var opponent = rootPlayer.Opponent;
-
-            // Check for a win/loss
-            if (state.PlayerWon != State.DRAW) {
-                return state.PlayerWon == rootPlayerId ? 1 : -1;
-            }
-
-            // Some thoughts:
-            //      Minion toughness + attack values
-            //          Complexity -> higher health is worth more when power is also higher
-            //      Some properties:
-            //          # cards in play
-            //          # cards in hand
-            //          # health points remaining
-            //          # cards in deck
-            //      Weapons should also be factored in
-            //      Determine TTL (turns til lethal) with those properties
-            //      TTL - opponent's TTL
-            //          Is that the metric we want to check?
-
-            // Note: these stats should be calculated from the root player's perspective.
-
-            // Get the player's minions and some stats about them
-            //var playerMinions = rootPlayer.BoardZone.GetAll();
-            //var playerMinionsAttack = playerMinions.Where(i => i.CanAttack).Sum(j => j.AttackDamage);
-            //var playerMinionsTauntHealth = playerMinions.Where(i => i.HasTaunt).Sum(j => j.Health);
-            //var opponentMinions = opponent.BoardZone.GetAll();
-            //var opponentMinionsAttack = opponentMinions.Where(i => i.CanAttack).Sum(j => j.AttackDamage);
-            //var opponentMinionsTauntHealth = opponentMinions.Where(i => i.HasTaunt).Sum(j => j.Health);
-
-            // Get some stats about player's resources
-            //var playerCardsInHand = rootPlayer.HandZone.Count;
-            //var opponentCardsInHand = opponent.HandZone.Count;
-            var playerHealth = rootPlayer.Hero.Health;
-            var opponentHealth = opponent.Hero.Health;
-            //var playerCardsInDeck = rootPlayer.DeckZone.Count;
-            //var opponentCardsInDeck = opponent.DeckZone.Count;
-            //var playerWeaponDamage = rootPlayer.Hero.Weapon?.AttackDamage * rootPlayer.Hero.Weapon?.Durability ?? 0;
-            //var opponentWeaponDamage = opponent.Hero.Weapon?.AttackDamage * opponent.Hero.Weapon?.Durability ?? 0;
-
-            // Now use these stats to come to a number....
-            //TODO implement Hearthstone state evaluation
-            return playerHealth == opponentHealth ? 0 : playerHealth > opponentHealth ? .5 : -.5;
-        }
-
         #endregion
 
     }
